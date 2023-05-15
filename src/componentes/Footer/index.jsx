09b6872e1827af4f3d5230fb1e7ed45cefb4225b
Manuel Lucero <cidorbleit@gmail.com>
2023-05-15 09:03:27 -0500

    org

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -3,13 +3,13 @@ import "./Footer.css"
 const Footer = () => {
     return <footer className='footer' style={{ backgroundImage: "url(/img/Footer.png)" }}>
         <div className='redes'>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://org-one-mu.vercel.app/'>
                 <img src="/img/facebook.png" alt='Facebook' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://org-one-mu.vercel.app/'>
                 <img src="/img/twitter.png" alt='twitter' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://org-one-mu.vercel.app/'>
                 <img src="/img/instagram.png" alt='instagram' />
             </a>
         </div>
