1b4878fa0c868b6546ae56e26655f694c30b26c9
Manuel Lucero <cidorbleit@gmail.com>
2023-05-29 19:06:27 -0500

    xd

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   {
     id: uuid(),
     equipo: "Padres",
-    foto: "https://pps.whatsapp.net/v/t61.24694-24/138452550_227780428828308_2573727897326841641_n.jpg?ccb=11-4&oh=01_AdTQE9ziAZkxfenS5Lxv8HaQEQJ_BdGycij_PU6KPPn8Vg&oe=6467FC0A",
+    foto: "https://images.unsplash.com/photo-1633412802994-5c058f151b66?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
     nombre: "Elizabeth Roque",
     puesto: "Madre",
     fav: false
@@ -40,7 +40,7 @@ function App() {
   {
     id: uuid(),
     equipo: "Hijos",
-    foto: "https://scontent.fcix2-1.fna.fbcdn.net/v/t39.30808-6/294985213_1981961055524988_3119078025123071701_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=174925&_nc_eui2=AeGO9q0yEUL8NbpBPf2PVeyLurHvZ9MmI0a6se9n0yYjRjCubSNbE1saVNcN41XvsaOKUZ1bqsI-pT5S1drI-Qi3&_nc_ohc=rqJsC38Sh_EAX9vgBb-&_nc_ht=scontent.fcix2-1.fna&oh=00_AfCe992CqL1LLYIkMVLRnHNY61stnXDSJEJKvw2EIlzaaw&oe=64670ACF",
+    foto: "https://images.unsplash.com/photo-1633412802994-5c058f151b66?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
     nombre: "Nattaly Lucero",
     puesto: "Hija",
 
@@ -48,7 +48,7 @@ function App() {
   {
     id: uuid(),
     equipo: "Yernos",
-    foto: "https://pps.whatsapp.net/v/t61.24694-24/321083082_741868134126363_2143311163724515268_n.jpg?ccb=11-4&oh=01_AdR25BS2AdUQnAfYPRBABoNEqWwrRIUzwoHTSRWP24-qlg&oe=646A5212",
+    foto: "https://images.unsplash.com/photo-1633412802994-5c058f151b66?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
     nombre: "Isabel Facho",
     puesto: "Esposa de Esteban Lucero",
     fav: false
@@ -56,7 +56,7 @@ function App() {
   {
     id: uuid(),
     equipo: "Yernos",
-    foto: "https://scontent.fcix2-1.fna.fbcdn.net/v/t39.30808-6/240114539_4245071322249487_3359242113906764310_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeFZaHcFU7l7Zb7cV5kkPZodUdelZUC6-kpR16VlQLr6Spm55ZNHIYTiTS3YWp48kLydMqTVX-ko-WN3EylbwhDJ&_nc_ohc=hsxA9c9t8fIAX-8sDYb&_nc_ht=scontent.fcix2-1.fna&oh=00_AfBeQgXmu9R_ouBDS6_NZiMYBJa1OEftn8XvxrmSA2qd-w&oe=6467A828",
+    foto: "https://images.unsplash.com/photo-1633412802994-5c058f151b66?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
     nombre: "Kevin Alcalde",
     puesto: "Esposo de Nattaly Hija",
     fav: false
@@ -64,7 +64,7 @@ function App() {
   {
     id: uuid(),
     equipo: "Nietos",
-    foto: "https://pps.whatsapp.net/v/t61.24694-24/346966045_1010231693261055_8550615474453696816_n.jpg?ccb=11-4&oh=01_AdS2SjmkiiyzBuY2pds_ebGoCW2NP83JgjpFKvwJHn3wag&oe=646F482D",
+    foto: "https://images.unsplash.com/photo-1633412802994-5c058f151b66?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
     nombre: "Sebastian Lucero",
     puesto: "Hijo de Esteban e Isabel",
     fav: false
@@ -72,7 +72,7 @@ function App() {
   {
     id: uuid(),
     equipo: "Nietos",
-    foto: "https://scontent.fcix2-1.fna.fbcdn.net/v/t39.30808-6/346832523_3331489483765890_3350048863627595446_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFmx3tveSejcPssQ4MQy3EnqPiA5A5kOv6o-IDkDmQ6_p8576KMSaEndpfx-bB0ujckdKrZltlq0sVtLlEA77IV&_nc_ohc=jnDLxzFqf10AX-TATkr&_nc_ht=scontent.fcix2-1.fna&oh=00_AfBPf-uAZhEb46ALoMjryMT98Mx0_pplglcLSVDs3VdpMg&oe=6467FB77",
+    foto: "https://images.unsplash.com/photo-1633412802994-5c058f151b66?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
     nombre: "Camila Alcalde",
     puesto: "Hija de Nattaly y Kevin",
     fav: false
